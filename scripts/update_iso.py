9051a8172f0a9710a31dd8418f05c301411961a3
trjahnke <trjahnke@gmail.com>
2021-08-12 14:57:10 -0500

    fixed float return

--- scripts/update_iso.py
+++ scripts/update_iso.py
@@ -30,8 +30,7 @@ def main():
 
 # Check if string is float number
 def isfloat(value): 
-    t = value.replace('.', '').isdigit()
-    return t
+    return value.replace('.', '').isdigit()
 
 # Get the latest ubuntu iso and downloading it to the iso folder
 def ubuntu():
