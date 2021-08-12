eb19b75a12e74382baa389f18781556302061f2a
trjahnke <trjahnke@gmail.com>
2021-08-12 12:07:35 -0500

    mac drive addition to setup

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A suite for an on the go technician, from iso to testing
 # Setup
 - Download and install Ventoy
 - Run Ventoy and use the option to leave space at end of drive (ie 45gb)
-- Make that runover space into a storage exFat partition
+- Make that runover space into a storage exFat partition naming it 'storage'
 - run a git clone of this repo and jot down the drive letters of ventoy and storage
 
 
--- scripts/configure.py
+++ scripts/configure.py
@@ -14,23 +14,32 @@ def get_win_drives():
     """
     Return a list of all available drives
     """
-    if sys.platform == 'win32':
-        import win32api
-        drives = win32api.GetLogicalDriveStrings()
-        drives = drives.split('\\\x00')[:-1]
-        return [d for d in drives if os.path.exists(d)]
-    else:
-        return []
+    import win32api
+    drives = win32api.GetLogicalDriveStrings()
+    drives = drives.split('\\\x00')[:-1]
+    return [d for d in drives if os.path.exists(d)]
+
 
 def get_linux_drives():
     """
     Return a list of all available drives
     * will need to distinguish between wsl and native
     """
-    if sys.platform == 'linux':
-        return [f'{dev.upper()}:' for dev in os.listdir('/mnt/') if dev]
-    else:
-        return []
+    return [f'{dev.upper()}:' for dev in os.listdir('/mnt/') if dev]
+
+
+# Get list of all mac drives
+def get_mac_drives():
+    """
+    Return string for storage volume to validate it is present
+    """
+    try:
+        for drive in os.listdir('/Volumes/'):
+            if os.path.isdir('/Volumes/storage'):
+                return drive
+    except:
+        print('No mac drive "storage" found')
+    
 
 def available_drives():
     """
@@ -41,18 +50,26 @@ def available_drives():
         return get_win_drives()
     elif platform == 'linux':
         return get_linux_drives()
+    elif platform == 'darwin':
+        return get_mac_drives()
     else:
         print('Operating system not supported')
 
-# Get user input for which drive to use
 while True:
-    drive_letter = input(f'{available_drives()} \nPlease select a drive letter for storage: ').upper()
-    if (f'{drive_letter}:') not in available_drives():
-        print('Invalid drive letter')
-        continue
+    # If user is coming from mac, auto-select the storage volume.
+    # If user is coming from windows or linux, have user select drive.
+    if os_name == 'darwin':
+        print(f'Mac detected \nAdding {available_drives()} to config.')
+        flash_drive = available_drives()
+        break
     else:
-        flash_drive = drive_letter
-        break       
+        drive_letter = input(f'{available_drives()} \nPlease select a drive letter for storage: ').upper()
+        if (f'{drive_letter}:') not in available_drives():
+            print('Invalid drive letter')
+            continue
+        else:
+            flash_drive = drive_letter
+            break       
 
 # Build out OS section of config.ini file
 config['BASE'] = {
